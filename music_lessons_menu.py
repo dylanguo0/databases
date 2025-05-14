94bd8f873c7548faff3ff6d26d5a04924bb9bac4
dylanguo0 <dylanguo@riccarton.school.nz>
2025-05-14 14:00:07 +1200

    Created the menu for all the queries

--- music_lessons_menu.py
+++ music_lessons_menu.py
@@ -22,4 +22,20 @@ def print_query(view_name:str):
     print(tabulate(results,headings))
     db.close()
 
-print_query('all_data')
+menu_choice = ''
+print('Welcome to the music lessons database')
+while menu_choice != 'EXIT':
+    menu_choice = input('\nType the number for the information you want:\n'
+                        '1: All music lessons\n'
+                        '2: All students sorted in alphabetical order by surname then first name\n'
+                        '3: All music lessons in order from earliest to latest\n'
+                        '4: All students who have a lesson on Monday, from earliest to latest\n'
+                        '5: All students who have a lesson on Wednesday, from earliest to latest\n'
+                        '6: All students who\'s learning the piano\n'
+                        '7: All male students\n'
+                        '8: Parents who owe the most money\n'
+                        '9: Students sorted from oldest to youngest\n'
+                        '10: All students born in 2008\n'
+                        'EXIT: To exit the menu\n\n'
+                        'Type option here: ')
+    menu_choice = menu_choice.upper()
